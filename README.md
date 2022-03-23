2432123723e612c8c69d32bb4fb9a9ebb7576dd0
Cheryl Ju <cheryl.as.ju@gmail.com>
2022-03-23 13:54:58 -0400

    Added links to ReadMe page and added information to JavaScript for the temperature.

--- README.md
+++ README.md
@@ -2,6 +2,11 @@
 Assignment 9 - Weather Widget for MMC6278 class
 
 ## Issues While Completing Assignment
-I was having issues with my input part of my from disappearing after pressing the submit button. So the user would have to refresh the page if they wanted to run another query. 
+I was having issues with my input part of my from disappearing after pressing the submit button. So the user would have to refresh the page if they wanted to run another query. We figured out I had the incorrect end tag in the html which fixed this issue.
+
+Another issue I am running into is my items are saying "undefined" on the website link after users put in a location.  I found a fix on the Stack Overflow link below.
 
 ## Outside Resources
+
+Help with "undefined" issue: https://stackoverflow.com/questions/41008123/openweather-api-getjson-request-returning-undefined
+Degree symbol for JavaScript: http://www.javascripter.net/faq/mathsymbols.htm
--- main.js
+++ main.js
@@ -7,7 +7,7 @@ form.onsubmit = function(functionEvent) {
   functionEvent.preventDefault()
   var usersInput = input.value
   if(!usersInput) return
-  fetch('https://api.openweathermap.org/data/2.5/weather?appid=51e49b626dc2924e1eeab8c2d1ddb3d5&q=' + usersInput)
+  fetch('https://api.openweathermap.org/data/2.5/weather?&units=imperial&appid=51e49b626dc2924e1eeab8c2d1ddb3d5&q=' + usersInput)
   .then(function(resultJson) {
       return resultJson.json();
   })
@@ -34,13 +34,25 @@ function weatherResult(weatherObject) {
 
   /* Description of Current Weather */
   var description = document.createElement('h3');
-  description.textContent = "Weather Description: " + weatherObject.weather.description;
+  description.textContent = "Current Weather: " + weatherObject.weather[0].description;
   weather.appendChild(description);
 
   /* Icon Image for Current Weather Conditions */
+  /*
+  var icon = document.createElement('img');
+  icon.textContent = weatherObject.weather[0].icon;
+  weather.appendChild(icon);  */
+
+
 
   /* Current Temperature */
+  var description = document.createElement('h3');
+  description.textContent = "Current Temperature: " + weatherObject.main.temp + " \xB0";
+  weather.appendChild(description);
 
   /* Current "feels like" Temperature */
+  var description = document.createElement('h3');
+  description.textContent = "Feels Like : " + weatherObject.main.feels_like + " \xB0";
+  weather.appendChild(description);
 
 }
